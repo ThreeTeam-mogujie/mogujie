e702fd0db0677dfa741feef1fcaf36dcd63f6235
yuzhigang <307390047@qq.com>
2017-04-24 21:46:26 +0800

    'add_summerGroup'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('mogujie',['ui.router','angularCSS','homeModule','categoryModule','shopcarModule','mineModule','groupBuyingModule'])
+angular.module('mogujie',['ui.router','angularCSS','homeModule','categoryModule','shopcarModule','mineModule','groupBuyingModule','summerBuyingModule'])
 .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/home');
 })
