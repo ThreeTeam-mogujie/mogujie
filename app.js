ae5d9363854c40669a6ffa20c73ce87f60cef73d
xuedeng <chenxuedeng@126.com>
2017-04-25 16:30:08 +0800

    'deng的一次尝试'

--- app.js
+++ app.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-angular.module('mogujie',['ui.router','angularCSS','homeModule','me-lazyload','categoryModule','shopcarModule','mineModule','groupBuyingModule','UZGroupModule'])
-=======
 angular.module('mogujie',['ui.router','angularCSS','me-lazyload','homeModule','categoryModule','shopcarModule','mineModule','groupBuyingModule','summerBuyingModule','UZGroupModule'])
->>>>>>> 834ba6134b4c15724a8b445fdd3753d1eda45eca
 .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/home');
 })
