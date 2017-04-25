834ba6134b4c15724a8b445fdd3753d1eda45eca
yuzhigang <307390047@qq.com>
2017-04-25 11:34:55 +0800

    'add_lazyload'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-angular.module('mogujie',['ui.router','angularCSS','homeModule','categoryModule','shopcarModule','mineModule','groupBuyingModule','summerBuyingModule','UZGroupModule'])
+angular.module('mogujie',['ui.router','angularCSS','me-lazyload','homeModule','categoryModule','shopcarModule','mineModule','groupBuyingModule','summerBuyingModule','UZGroupModule'])
 .config(function($stateProvider, $urlRouterProvider){
     $urlRouterProvider.otherwise('/home');
 })
