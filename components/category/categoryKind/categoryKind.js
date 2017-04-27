b9a8131fd3f45bcb8b42f7a82a78235ad835e6da
zhengwenf <944614289@qq.com>
2017-04-27 20:54:52 +0800

    '添加了商品详细信息'

--- components/category/categoryKind/categoryKind.js
+++ components/category/categoryKind/categoryKind.js
@@ -1,4 +1,4 @@
-angular.module('categoryKindModule',[])
+angular.module('categoryKindModule',["categoryKindDetailModule"])
 .config(function($stateProvider, $urlRouterProvider){
 	$stateProvider
 		.state('category.categoryKind',{
@@ -10,9 +10,97 @@ angular.module('categoryKindModule',[])
 		})
 })
 
-.controller('categoryKindCon',['$scope','$http',function($scope,$http){
+
+
+	
+.controller('categoryKindCon',['$scope','$http','$timeout','myFactory','$rootScope',function($scope,$http,$timeout,myFactory,$rootScope){
+	//从category页面传值过来
+	
+	if(myFactory.get("rightUpItem")){
+		$rootScope.categoryKindItem = myFactory.get("rightUpItem");
+		sessionStorage.setItem("categoryKindItem",JSON.stringify(myFactory.get("rightUpItem")));
+	}else{
+		$rootScope.categoryKindItem = JSON.parse(sessionStorage.getItem("categoryKindItem"));
+	}
+	
+
+//	为四个分类添加不同事
+	categoryKindTitleClick($scope,$http,$rootScope.categoryKindItem,$timeout);
+	
+	$scope.toCategoryKindDetail=function(categoryKindImg){
+		sessionStorage.setItem("categoryKindDetailItem",JSON.stringify(categoryKindImg));
+	}
 	
 }])
 
 
 
+function categoryKindTitleClick($scope,$http,categoryKindItem,$timeout){
+	var fcid=getSubStringNum(categoryKindItem.link,"fcid=");
+	var acm =getSubStringNum(categoryKindItem.link,"acm=");
+	initCategoryKindWaterfall($scope,$http,fcid,$timeout,"pop",acm,categoryKindItem.title);	
+
+	$scope.categoryKindTitleZh=function($event){
+		$scope.categoryKindTitleSelected=1;	
+		initCategoryKindWaterfall($scope,$http,fcid,$timeout,"pop",acm,categoryKindItem.title);
+	}
+	
+	$scope.categoryKindTitleXl=function($event){
+		$scope.categoryKindTitleSelected=2;
+		initCategoryKindWaterfall($scope,$http,fcid,$timeout,"sell",acm,categoryKindItem.title);
+	}
+	
+	$scope.categoryKindTitleXp=function($event){
+		$scope.categoryKindTitleSelected=3;
+		initCategoryKindWaterfall($scope,$http,fcid,$timeout,"new",acm,categoryKindItem.title);
+	}
+	
+	$scope.categoryKindTitleJe=function($event){
+		$scope.categoryKindTitleSelected=4;
+
+	}
+}
+
+
+function getSubStringNum(string,subString){
+		var start = string.indexOf(subString);
+		var s = string.substring(start);	
+		var end = s.indexOf("&");
+		if(end<=0){
+			end = s.length;
+		}
+		var ss=s.substring(0,end);
+		var startNext = ss.indexOf("=");
+		return (ss.substring(startNext+1));
+}
+
+
+//加载瀑布流数据
+
+function initCategoryKindWaterfall($scope,$http,ficd,$timeout,sort,acm,title){
+
+		var page=1;	
+				$http.jsonp('http://list.mogujie.com/search?page='+page+'$title='+title+'&sort='+sort+'&acm='+acm+'&fcid='+ficd+'&cKey=16&callback=JSON_CALLBACK').success(function(allProduts){
+					 $scope.categoryKindImages=allProduts.result.wall.docs;
+//					 console.log("categoryKind"+allProduts);
+					 $scope.categoryKindHotCates=allProduts.result.hotCates;
+
+				})
+				
+		$scope.loadMoreData = function(){
+            $timeout(function(){
+                page++;
+                $http.jsonp('http://list.mogujie.com/search?page='+page+'&sort='+sort+'&acm='+acm+'&fcid='+ficd+'&cKey=16&callback=JSON_CALLBACK').success(function(allProduts){					 
+					 for (var i = 0; i < allProduts.result.wall.docs.length; i++) {
+                        $scope.categoryKindImages.push(allProduts.result.wall.docs[i]);
+                 	}					 
+				})
+
+            },10);
+        };
+        
+	 	$scope.$on("waterfall:loadMore",function(){//滚动自动填充事件
+              $scope.loadMoreData();
+          })
+		
+};
---
+++ components/category/categoryKind/categoryKindDetail/categoryKindDetail.js
@@ -0,0 +1,62 @@
+angular.module('categoryKindDetailModule',[])
+.config(function($stateProvider, $urlRouterProvider){
+	$stateProvider
+		.state('category.categoryKind.categoryKindDetail',{
+			url:'/categoryKindDetail',
+			templateUrl:'components/category/categoryKind/categoryKindDetail/categoryKindDetail.html',
+			controller:'categoryKindDetailCon',
+			css:['components/category/categoryKind/categoryKind.css','components/category/categoryKind/categoryKindDetail/categoryKindDetail.css']
+
+		})
+})
+
+
+
+
+
+	
+.controller('categoryKindDetailCon',['$scope','$http','$timeout','$rootScope',function($scope,$http,$timeout,$rootScope){
+	initCategoryKindDetailSwiper($timeout);
+	$scope.categoryKindDetailItem = JSON.parse(sessionStorage.getItem("categoryKindDetailItem"));
+	if($scope.categoryKindDetailItem.acm){
+		if($scope.categoryKindDetailItem.tradeItemId){
+			var iid=$scope.categoryKindDetailItem.tradeItemId;		
+		}else{
+			var iid = $scope.categoryKindDetailItem.iid;
+		}
+		
+		$http.get('http://m.mogujie.com/detail/mgj/v1/h5?_ajax=1&iid='+iid+'&cparam=').success(function(produts){
+					$scope.rightMenuCategoryDetailArr=produts.result;
+					console.log(produts);
+
+		})
+	}
+}])
+
+
+function initCategoryKindDetailSwiper($timeout){
+//	new Swiper ('.swiper-container', {
+//              loop: true,
+//              autoplay:500,              
+//              initialSlide: 0,
+//              paginationType : 'fraction',
+//              // 如果需要分页器
+//              pagination: '.swiper-pagination',
+//               observer:true,
+//              observeParents:true,
+//
+//      })
+	
+	
+	 $timeout(function(){
+         new Swiper ('.swiper-container', {
+                loop: true,
+                autoplay:false,           
+                paginationType : 'fraction',
+                // 如果需要分页器
+//				allowSwipeToNext:false,
+                pagination: '.swiper-pagination',
+            })
+        },50);
+
+}
